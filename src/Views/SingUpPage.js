48cb47e9c9fda6e4e7163e39d0c9f26ba7f81eb3
Tina Safar Abadi <tina.safari85@gmail.com>
2021-06-03 11:33:01 -0400

    sign up page fixed

--- src/Views/SingUpPage.js
+++ src/Views/SingUpPage.js
@@ -3,7 +3,6 @@ import { Button, Form, Col } from "react-bootstrap";
 import { createUser } from "../fetchRequest";
 import { useStore, SIGNUP } from "../Store/store";
 import { useHistory } from "react-router-dom";
-import { FormErrors } from "./FormErrors";
 
 function SignUpPage() {
   const dispatch = useStore((state) => state.dispatch);
@@ -14,74 +13,8 @@ function SignUpPage() {
     lastName: "",
     email: "",
     password: "",
-    creditBalance: 100.0,
-    formErrors: { firstName: "", lastName: "", email: "", password: "" },
-    firstNameValid: false,
-    lastNameValid: false,
-    emailValid: false,
-    passwordValid: false,
-    formValid: false,
+    creditBalance: 50.0,
   });
-
-  const handleChange = (e) => {
-    const inputName = e.target.name;
-    const inputValue = e.target.value;
-
-    validateField(inputName, inputValue);
-    setUserdata((state) => ({ ...state, [inputName]: inputValue }));
-  };
-
-  const validateField = (fieldName, value) => {
-    let fieldValidationErrors = userData.formErrors;
-    let firstNameValid = userData.firstNameValid;
-    let lastNameValid = userData.lastNameValid;
-    let emailValid = userData.emailValid;
-    let passwordValid = userData.passwordValid;
-    switch (fieldName) {
-      case "firstName":
-        firstNameValid = value.length >= 2;
-        fieldValidationErrors.firstName = firstNameValid ? "" : " is too short";
-        break;
-      case "lastName":
-        lastNameValid = value.length >= 2;
-        fieldValidationErrors.lastName = lastNameValid ? "" : " is too short";
-        break;
-      case "email":
-        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-        fieldValidationErrors.email = emailValid ? "" : " is invalid";
-        break;
-      case "password":
-        passwordValid = value.length >= 5;
-        fieldValidationErrors.password = passwordValid ? "" : " is too short";
-        break;
-      default:
-        break;
-    }
-    setUserdata(
-      {
-        formErrors: fieldValidationErrors,
-        firstNameValid: firstNameValid,
-        lastNameValid: lastNameValid,
-        emailValid: emailValid,
-        passwordValid: passwordValid,
-      },
-      validateForm
-    );
-  };
-
-  const validateForm = () => {
-    setUserdata({
-      formValid:
-        userData.firstNameValid &&
-        userData.lastNameValid &&
-        userData.emailValid &&
-        userData.passwordValid,
-    });
-  };
-
-  const errorClass = (error) => {
-    return error.length === 0 ? "" : "has-error";
-  };
 
   const handleSubmit = (e) => {
     const reRoute = (e) => history.push("/");
@@ -99,8 +32,14 @@ function SignUpPage() {
       userData.password
     );
     dispatch({ type: SIGNUP, payload: userData });
-
     reRoute();
+  };
+
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+
+    setUserdata((state) => ({ ...state, [inputName]: inputValue }));
   };
 
   return (
@@ -111,16 +50,9 @@ function SignUpPage() {
         onSubmit={handleSubmit}
         style={{ margin: "auto", width: "700px", paddingBottom: "20px" }}
       >
-        
         <Form.Row>
           <Col>
             <Form.Group as={Col}>
-            <div
-                className={`form-group ${errorClass(
-                  userData.formErrors.firstName
-                )}`
-              }
-              ></div>
               <Form.Control
                 type="text"
                 name="firstName"
@@ -129,17 +61,10 @@ function SignUpPage() {
                 value={userData.firstName}
                 required
               />
-              
-              
             </Form.Group>
           </Col>
           <Col>
             <Form.Group as={Col}>
-              <div
-                className={`form-group ${errorClass(
-                  userData.formErrors.lastName
-                )}`}
-              ></div>
               <Form.Control
                 type="text"
                 name="lastName"
@@ -155,39 +80,28 @@ function SignUpPage() {
         <Form.Row>
           <Form.Group as={Col} controlId="formGridEmail">
             <Form.Label>Email</Form.Label>
-            <div
-              className={`form-group ${errorClass(userData.formErrors.email)}`}
-            >
-              <Form.Control
-                type="email"
-                name="email"
-                placeholder="Enter email"
-                value={userData.email}
-                required
-                onChange={handleChange}
-              />
-            </div>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              value={userData.email}
+              required
+              onChange={handleChange}
+            />
           </Form.Group>
         </Form.Row>
 
         <Form.Row>
           <Form.Group as={Col} controlId="formGridPassword">
             <Form.Label>Password</Form.Label>
-            <div
-              className={`form-group ${errorClass(
-                userData.formErrors.password
-              )}`}
-            >
-              {/* <div style={{color: "red"}}></div> */}
-              <Form.Control
-                type="password"
-                name="password"
-                placeholder="Password"
-                value={userData.password}
-                required
-                onChange={handleChange}
-              />
-            </div>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={userData.password}
+              required
+              onChange={handleChange}
+            />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formGridPassword">
@@ -195,12 +109,12 @@ function SignUpPage() {
             <Form.Control type="password" placeholder="Re-Enter Password" />
           </Form.Group>
         </Form.Row>
-        <div className="panel panel-default"
-        style={{color: 'red'}}>
-          <FormErrors formErrors={userData.formErrors} />
-        </div>
-        <Button variant="outline-dark" type="submit">
-          Sign up
+
+        <Button
+          variant="outline-dark"
+          type="submit"
+        >
+          Submit
         </Button>
       </Form>
     </>
